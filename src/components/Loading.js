ff922787263802ff131cb79957731ea5e2f8a455
Amir Sanni <amirs@wakanow.com>
2019-11-19 11:59:19 +0100

    Update home url

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Loading extends React.Component{
     render(){
         return (
-            <div className='container'>
-                <div className='row'>
+            <div className='container mt-5 pt-5'>
+                <div className='row mt-5 pt-5'>
                     <div className='col-12 text-center'>
                         <div className="spinner-grow text-white" role="status">
                             <span className="sr-only">Loading...</span>
