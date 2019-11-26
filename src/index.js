5912bc6ac0aeaafca6e49dafb5cba8b1cd467f63
Amir Sanni <amirs@wakanow.com>
2019-11-26 10:14:03 +0100

    Remove unused imported scripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'jquery/dist/jquery';
 import './css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
 import 'font-awesome/css/font-awesome.min.css'
 import Loading from './components/Loading';
 import Navbar from './components/Navbar';
