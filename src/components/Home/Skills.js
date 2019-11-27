4aca09eec6deb87eee8d565af85dc4f4edf9cfa0
Amir Sanni <amirsanni@gmail.com>
2019-11-27 22:30:42 +0100

    Update skills component by including URL

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Skills extends React.Component{
     render(){
         return (
-            this.props.skills.map((skill)=><button key={skill} className='btn btn-outline-light btn-sm skill mb-2'>{skill}</button>)
+            this.props.skills.map((skill)=>
+                <button key={skill.name} className='btn btn-outline btn-sm skill mb-2 no-hover' title={skill.url}>
+                    <a href={skill.url} className='text-decoration-none text-white' target='_blank' rel='noopener noreferrer'>{skill.name}</a>
+                </button>
+            )
         );
     }
 }
