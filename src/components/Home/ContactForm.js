aab9d0185165ca287d6b24d80724c24d6c972a7a
Amir Sanni <amirsanni@gmail.com>
2019-12-18 09:25:08 +0100

    Implement contact form

--- src/components/Home/ContactForm.js
+++ src/components/Home/ContactForm.js
@@ -8,8 +8,74 @@ export default class ContactForm extends React.Component{
             name: '',
             email: '',
             msg: '',
-            progressMsg: ''
+            progressMsg: '',
+            progressMsgClass: 'text-info'
         };
+    }
+
+
+    handleStateChange = (e)=>{
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+
+    sendMessage = ()=>{
+        if(this.state.name.trim() && this.state.email.trim() && this.state.msg.trim()){
+            this.setState({progressMsg: 'Delivering your message. Please wait....'});
+
+            fetch(process.env.REACT_APP_API_URL+'?action=send_email', {
+                method: 'POST',
+                mode:'cors',
+                body: {
+                    msg: this.state.msg,
+                    name: this.state.name,
+                    email: this.state.email
+                }
+            }).then((response)=>{
+                if(response.ok){
+                    return response.json();
+                }
+
+                throw new Error('Oops! I must have missed my way. Couldn\'t deliver your message at this time. Please try again.');
+            }).then((rd)=>{
+                if(rd.data.status === 1){
+                    this.setState({
+                        progressMsg: 'Your message has been delivered. I\'ll get back to you in due time. <br> Thank you.',
+                        progressMsgClass: 'text-success'
+                    });
+
+                    setTimeout(()=>{
+                        this.clearFields();
+                    }, 3000);
+                }
+
+                else{
+                    this.setState({
+                        progressMsg: rd.data.msg ? rd.data.msg : 'Sorry, your message could not be delivered at this time. Please try again later.',
+                        progressMsgClass: 'text-danger'
+                    });
+                }
+            }).catch((err)=>{
+                this.setState({
+                    progressMsg: err,
+                    progressMsgClass: 'text-danger'
+                });
+            });
+        }
+
+        else{
+            this.setState({
+                progressMsg: "All fields are required!",
+                progressMsgClass: 'text-danger'
+            });
+        }
+    }
+
+
+    clearFields = ()=>{
+        for(let s in this.state){
+            this.setState({[s]: ''});
+        }
     }
 
 
@@ -28,21 +94,40 @@ export default class ContactForm extends React.Component{
                                     Have an offer, a question or just want to say hi? Shoot!
                                 </div>
 
+                                <div className={`col-12 text-center h6 font-weight-bold mb-4 small ${this.state.progressMsgClass}`}
+                                    dangerouslySetInnerHTML={{__html:this.state.progressMsg}}>
+                                </div>
+
                                 <div className='col-md-6'>
                                     <label className='font-weight-lighter'>Your Name:</label>
-                                    <input type='text' placeholder='Your Name' className='form-control rounded-0 border-0' />
-                                    <span className='form-text'></span>
+                                    <input type='text' 
+                                        placeholder='Your Name' 
+                                        className='form-control rounded-0 border-0' 
+                                        id='name'
+                                        value={this.state.name}
+                                        onChange={this.handleStateChange}
+                                    />
                                 </div>
 
                                 <div className='col-md-6'>
                                     <label className='font-weight-lighter'>Your Email:</label>
-                                    <input type='email' placeholder='Your Email' className='form-control rounded-0 border-0' />
-                                    <span className='form-text'></span>
+                                    <input type='email' placeholder='Your Email' className='form-control rounded-0 border-0'
+                                        id='email'
+                                        value={this.state.email}
+                                        onChange={this.handleStateChange}
+                                    />
                                 </div>
 
                                 <div className='col-12 mt-3'>
                                     <label className='font-weight-lighter'>Your Message:</label>
-                                    <textarea className='form-control rounded-0 border-0' rows='5' placeholder='Your Message...'></textarea>
+                                    <textarea className='form-control rounded-0 border-0' rows='5' placeholder='Your Message...'
+                                        id='msg'
+                                        value={this.state.msg}
+                                        onChange={this.handleStateChange}></textarea>
+                                </div>
+
+                                <div className='col-12 mt-3'>
+                                    <button className='btn btn-block rounded-0 app-btn-alt' onClick={this.sendMessage}>Send Message</button>
                                 </div>
                             </div>
                         </div>
